82a134b256cfd34b8719e176519d037919a3768c
LongWalkAhead <bluelama1234@gmail.com>
2024-11-17 17:57:08 -0500

    Add files via upload

---
+++ proj2.py
@@ -0,0 +1,7 @@
+#task 1
+User = input("input 3 numbers!") 
+
+numbers = 3,4,5
+#task 2
+if numbers is 5 or 3: 
+    print("the biggest number is 5, the smallest number is 3")
---
+++ proj3.py
@@ -0,0 +1,7 @@
+x="leap year"
+result = x/400 /100
+result = input("2000")
+if result == x:
+    print("this is a leap year!")
+elif result != x :
+    print("this is not a leap year!")
